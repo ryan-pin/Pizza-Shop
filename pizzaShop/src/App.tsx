469e4205c2f118245b41e5976aec2c4088bc5a49
Ryan <ryan.piinheiro@hotmail.com>
2024-12-23 21:18:18 -0300

    configuração inicial shadcn

---
+++ pizzaShop/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1>Pizza Shop</h1>
+        <Button>Começar</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
