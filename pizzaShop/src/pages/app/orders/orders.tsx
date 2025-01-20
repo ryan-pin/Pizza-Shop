27ec3bdad49e9493228c48832f2a4111f496fa72
Ryan <ryan.piinheiro@hotmail.com>
2025-01-20 19:47:42 -0300

    componente paginação

--- pizzaShop/src/pages/app/orders/orders.tsx
+++ pizzaShop/src/pages/app/orders/orders.tsx
@@ -1,41 +1,50 @@
-import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Helmet } from "react-helmet-async";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilter } from "./order-table-filter";
+import { Pagination } from "@/components/pagination";
 
 export function Orders() {
-    return (
-        <>
-            <Helmet title="Pedidos"/>
-            <div className="flex flex-col gap-4">
-                <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
-            </div>
-            <div className="space-y-2.5">
-                <OrderTableFilter />
-                <div className="border rounded-md">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[64px]"></TableHead>
-                                <TableHead className="w-[148px]">ID</TableHead>
-                                <TableHead className="w-[180px]">Realizado há</TableHead>
-                                <TableHead className="w-[140px]">Status</TableHead>
-                                <TableHead>Cliente</TableHead>
-                                <TableHead className="w-[140px]">Total pedido</TableHead>
-                                <TableHead className="w-[164px]"></TableHead>
-                                <TableHead className="w-[132px]"></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {Array.from({ length: 10 }).map((_, i) => {
-                                return (
-                                    <OrderTableRow key={i}/>
-                                )
-                            })}
-                        </TableBody>
-                    </Table>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
+        <div className="space-y-2.5">
+          <OrderTableFilter />
+          
+          <div className="border rounded-md">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[148px]">ID</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />;
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={50} perPage={10} />
+        </div>
+      </div>
+    </>
+  );
 }
