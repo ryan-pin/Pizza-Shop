5ecd829c1dd36de56a9ec6d1be548bfd0e6d6007
Ryan <ryan.piinheiro@hotmail.com>
2025-01-20 20:55:13 -0300

    componente de detalhes

--- pizzaShop/src/pages/app/orders/order-table-row.tsx
+++ pizzaShop/src/pages/app/orders/order-table-row.tsx
@@ -1,23 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="sm">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="sm">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-sm font-medium">4584865</TableCell>
       <TableCell className="text-muted-foreground">há x minutos</TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span className="font-medium text-muted-foreground">Pendente</span>
-        </div>
+
       </TableCell>
       <TableCell className="font-medium">Fabiano Justo</TableCell>
       <TableCell className="font-medium">R$ 50,00</TableCell>
