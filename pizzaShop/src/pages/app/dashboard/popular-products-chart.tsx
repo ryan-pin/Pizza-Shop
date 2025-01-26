3befa89f38c3f3dbbab63b8716ec13c002abdebd
Ryan <ryan.piinheiro@hotmail.com>
2025-01-26 17:27:44 -0300

    grafico de pizza

---
+++ pizzaShop/src/pages/app/dashboard/popular-products-chart.tsx
@@ -0,0 +1,91 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { BarChart } from "lucide-react";
+
+import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
+
+import colors from "tailwindcss/colors";
+
+const data = [
+  { product: "Pizza de Frango", amount: 40 },
+  { product: "Pizza de 4 Queijos", amount: 30 },
+  { product: "Pizza Portuguesa", amount: 50 },
+  { product: "Pizza de Carne", amount: 40 },
+];
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
+];
+
+export function PopularProductsChart() {
+  return (
+    <Card className="col-span-3">
+      <CardHeader className="pb-8">
+        <div className="flex items-center justify-between">
+          <CardTitle className="text-base font-medium">
+            Produtos populares
+          </CardTitle>
+          <BarChart className="w-4 h-4 text-muted-foreground" />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <PieChart style={{ fontSize: 12 }}>
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+              
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index]}
+                    className="stroke-background hover:opacity-75"
+                    
+                  />
+                );
+              })}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+}
--- pizzaShop/src/pages/app/dashboard/revenue-chart.tsx
+++ pizzaShop/src/pages/app/dashboard/revenue-chart.tsx
@@ -13,7 +13,7 @@ import {
   YAxis,
   CartesianGrid,
   Line,
-  Tooltip,
+
 } from "recharts";
 
 const data = [
@@ -41,18 +41,19 @@ export function RevenueChart() {
         <ResponsiveContainer width="100%" height={240}>
           <LineChart data={data} style={{ fontSize: 12 }}>
             <XAxis tickLine={false} dataKey="date" />
-            
+
             <YAxis
               stroke="#888"
               dataKey="revenue"
               tickLine={false}
               tickFormatter={(value: number) =>
-                value.toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
+                value.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
                 })
               }
             />
+            <CartesianGrid vertical={false} className="stroke-muted" />
             <Line type="linear" dataKey="revenue" strokeWidth={2} />
           </LineChart>
         </ResponsiveContainer>
