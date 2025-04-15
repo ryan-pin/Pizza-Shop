b26582c6cf5c3e5594ae34ac5523a22642b6d578
Ryan <ryan.piinheiro@hotmail.com>
2025-04-15 20:59:47 -0300

    autenticação do usuario

--- pizzaShop/src/App.tsx
+++ pizzaShop/src/App.tsx
@@ -5,6 +5,8 @@ import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
         <ThemeProvider storageKey="Pizza-theme" defaultTheme="system">
           <Helmet titleTemplate="%s | pizza.shop" />
           <Toaster richColors />
-          <RouterProvider router={router} />
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
         </ThemeProvider>
       </HelmetProvider>
     </>
